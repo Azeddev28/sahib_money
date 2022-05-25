d676b323b85e427be8f247e111a893cad55bc123
Ahsan Zahid <ahsanzahid@A006-00517.local>
2022-05-25 15:44:38 +0500

    Initial setup

---
+++ config/settings/local.py
@@ -0,0 +1,12 @@
+from config.settings.base import *
+
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
+}
