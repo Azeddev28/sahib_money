0b0e82d9b628d85251d3e697a55e6357f5c74069
Hasnain Saeed <69143815+hasnain-saeed@users.noreply.github.com>
2022-05-26 19:17:54 +0500

    Fix conf settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,5 +1,16 @@
 from config.settings.base import *
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sahibmoney',
+        'USER': 'django',
+        'PASSWORD': 'ddd836a86c65b57c439728991489f341',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'mail.privateemail.com'
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
