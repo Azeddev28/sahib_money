847b2d60221991c673f243387a646a43877f0242
Hasnain Saeed <69143815+hasnain-saeed@users.noreply.github.com>
2022-05-30 19:54:58 +0500

    Add wallet app

--- config/wsgi.py
+++ config/wsgi.py
@@ -3,7 +3,8 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
+from decouple import config
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.' + config('ENVIRONMENT', default='local'))
 
 application = get_wsgi_application()
