f4fc5cc0b8e10657792569558c2e4d3544b8e492
Haider Tariq <haider.tariq@arbisoft.com>
2022-06-07 19:12:54 +0500

    kuss

--- apps/third_party_transaction/views.py
+++ apps/third_party_transaction/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.views import View
 
+from apps.third_party_transaction.models import ThirdPartyTransaction, TransactionOTP
+
 
 class OTPView(View):
     def get(self, request, *args, **kwargs):
+        user = request.user
+        transaction_uuid = request.GET.get('uuid')
+        try:
+            transaction = ThirdPartyTransaction.objects.get(uuid=transaction_uuid)
+        catch ThirdPartyTransaction.DoesNotExist:
+            
+        if user != transaction.wallet.user:
+            # user ki phir kuss paar do
+            pass
+
+        otp = TransactionOTP.objects.create(transaction=transaction)
+
+        # user ko email men bhej do aync task men
+
+
+    def post(self, request, *args, **kwargs):
+        # verify otp
         pass
