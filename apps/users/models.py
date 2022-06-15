7b3c4cdc2f4148ac79671c142ec957da44134c52
Haider Tariq <haider.tariq@arbisoft.com>
2022-06-15 18:29:04 +0500

    model changed, form added on profile

--- apps/users/models.py
+++ apps/users/models.py
@@ -58,7 +58,7 @@ class MerchantAccount(BaseModel):
     merchant_account_name = models.CharField(max_length=64)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='merchant_account')
     company_website = models.CharField(max_length=200)
-
+    redirect_url = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         return self.merchant_account_name
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponseRedirect
+
+from .forms import MerchantAccountForm
+from .models import MerchantAccount
 
 
 class ProfileDetailsView(View):
@@ -8,6 +12,15 @@ class ProfileDetailsView(View):
 
     def get(self, request, *args, **kwargs):
         if getattr(request.user, 'merchant_account', None):
-            return render(request, self.merchant_profile_template, {})
+            merchant = MerchantAccount.objects.get(user=request.user.id)
+            form = MerchantAccountForm(instance=merchant)
+            return render(request, self.merchant_profile_template, {'form': form})
 
         return render(request, self.user_profile_template, {})
+
+    def post(self, request, *args, **kwargs):
+        merchant = MerchantAccount.objects.get(user=request.user.id)
+        form = MerchantAccountForm(request.POST, instance=merchant)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
